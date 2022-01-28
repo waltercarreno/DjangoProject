3fcd6da7a5887730d01f5915ccd3280407faa9a1
waltercarreno <walter_tavernes14@hotmail.com>
2022-01-28 19:01:12 +0000

    Product.html created and paginate added to views

--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     sku = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField()
 
--- products/views.py
+++ products/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Product
 
-# Create your views here.
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
-
-    products = Product.objects.all()
+    """ Views from products, query selector and pagination """
+    
+    products = Product.objects.all() 
+    paginator = Paginator(products, 1)
+    number_page = request.GET.get('page')
+    obj_page = paginator.get_page(number_page)
 
     context = {
-        'products': products,
+        'products': obj_page,
     }
-
+    
     return render(request, 'products/products.html', context)
