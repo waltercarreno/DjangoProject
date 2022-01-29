a54e3b77eecdcf889cebe8678679f26921c498a9
waltercarreno <walter_tavernes14@hotmail.com>
2022-01-29 11:22:52 +0000

    Created product detail and solve css problems

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Product
 
@@ -16,3 +16,15 @@ def all_products(request):
     }
     
     return render(request, 'products/products.html', context)
+
+
+def product_page(request, product_id):
+    """ Views from product especific id"""
+    
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_id.html', context)
