66e6d6ed33570185c45793d8a855e8fd0619a15b
waltercarreno <walter_tavernes14@hotmail.com>
2022-01-29 16:22:01 +0000

    Solved problem with pagination

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -7,15 +9,28 @@ def all_products(request):
     """ Views from products, query selector and pagination """
     
     products = Product.objects.all() 
+    query = request.GET.get('q')
+
+    if query:
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
     paginator = Paginator(products, 1)
-    number_page = request.GET.get('page')
-    obj_page = paginator.get_page(number_page)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
 
     context = {
-        'products': obj_page,
+        'products': products,
     }
     
     return render(request, 'products/products.html', context)
+
+
+
 
 
 def product_page(request, product_id):
