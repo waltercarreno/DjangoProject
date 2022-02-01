c196b4cecbb0cfe5af4e29d027b7c0b35bf04c5e
waltercarreno <walter_tavernes14@hotmail.com>
2022-02-01 17:03:02 +0000

    Changes to navbar, pagination and solves bugs with order products and sorting.

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_page, name='product_id'),
+    path('<id>', views.product_page, name='product_id'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,19 +2,47 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
+
 
 
 def all_products(request):
-    """ Views from products, query selector and pagination """
-    
-    products = Product.objects.all() 
-    query = request.GET.get('q')
+    """ A view to show all products, including sorting and search queries """
 
-    if query:
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
-    paginator = Paginator(products, 1)
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     try:
         products = paginator.page(page)
@@ -25,21 +53,26 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
 
 
 
-def product_page(request, product_id):
+def product_page(request, id):
     """ Views from product especific id"""
     
-    product = get_object_or_404(Product, pk=product_id)
+    product=Product.objects.get(id=id)
+    related_products=Product.objects.filter(category=product.category).exclude(id=id)[:4]
 
     context = {
         'product': product,
+        'related_products': related_products,
     }
 
     return render(request, 'products/product_id.html', context)
