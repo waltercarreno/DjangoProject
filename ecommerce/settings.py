bb531962b60d0deb9444457cf4f546f4f30b36fb
waltercarreno <walter_tavernes14@hotmail.com>
2022-02-17 08:58:56 +0000

    Updated admin checkout and views

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
